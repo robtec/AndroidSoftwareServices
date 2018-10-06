23a381036174a8d6d275e43fda1100d7547133b3
Rob Powell <rob@robpowell.engineering>
2018-10-06 19:03:15 +0100

    added google

--- bloatware/remove_bloat.sh
+++ bloatware/remove_bloat.sh
@@ -13,14 +13,14 @@ adb version >/dev/null 2>&1 || { echo >&2 "$no_adb_error"; exit 1; }
 
 adb shell echo hello >/dev/null 2>&1 || { echo >&2 "$no_adb_device_error"; exit 1; }
 
-echo "using file $1"
+echo "Using package file $1"
 
 for p in $(cat $1)
 do
     pkg=$(echo $p | awk -F '#' '{print $1}')
     desc=$(echo $p | awk -F '#' '{print $2}')
 
-    echo "removing $desc - $pkg"
+    echo "Removing App $desc - $pkg"
 	adb shell pm uninstall -k --user 0 $pkg
     sleep 1
 done
